b1eedfca282e1a8aec931c8027c292f473899420
Ayush Chauhan <ayushchauhan.2076@gmail.com>
2026-08-06 01:26:00 +0530

    Configured Environment Variables
    Added .env and .env.local, configuring environment variables for MongoDB and CORS.

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,16 @@ import vaultentry from './models/vaultentry.js';
 
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors({
-    origin: 'http://localhost:3000' //allows requests only from this port
+    origin: `${process.env.CORS_ORIGIN || '*'}` //allows HTTP requests only from this port
 }))
 
 // connecting to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/vaultkey').then(()=> console.log('Connected to MongoDB')).catch((err)=> console.log('MongoDB connection error: ', err));
+mongoose.connect(`${process.env.MONGODB_URI}`).then(()=> console.log('Connected to MongoDB')).catch((err)=> console.log('MongoDB connection error: ', err));
 
 app.get('/', (req, res) => {
   res.send('This is VaultKey');
@@ -52,8 +52,8 @@ app.get('/vault', async (req, res) => {
 app.get('/vault/:id', async(req, res)=>{
   try{
     const entry = await vaultentry.findById(req.params.id);
-    if(!entry){res.status(404).json({message: "No entry found" })};
-    res.status(200).json(entry);
+    if(!entry){return res.status(404).json({message: "No entry found" })};
+    return res.status(200).json(entry);
   }catch(err){
     res.status(500).json({message: "Something went wrong"})
   }
@@ -103,7 +103,7 @@ app.put('/vault/:id', async(req, res)=>{
     // we pass runValidators:true to make sure Mongoose checks all schema rules (like required: true, maxLength: 50, min: 5, etc.) for the updated entry.
 
     if(!updateentry){
-      res.status(404).json({message:"Entry not found"})
+      return res.status(404).json({message:"Entry not found"})
     }
 
     console.log("Updated Entry: ", updateentry);
